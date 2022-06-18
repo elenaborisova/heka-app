eea2c9a798e672569184bf8bfd562f1c15c6249a
Elena Borisova <elenaborrisova@gmail.com>
2022-06-18 19:10:01 +0200

    test data display

--- static/js/index-chart.js
+++ static/js/index-chart.js
@@ -181,7 +181,77 @@ async function funDiagnosisRisk() {
         }
     });
 
-    const diagnosisAndRisk = await funDiagnosisRisk();
+
+    //Line Chart
+    var ctx = document.getElementById('line-chart').getContext("2d");
+    var gradientStroke = ctx.createLinearGradient(0, 0, 0, 450);
+    gradientStroke.addColorStop(0, '#009efb');
+
+    var gradientFill = ctx.createLinearGradient(0, 0, 0, 450);
+    gradientFill.addColorStop(0, "rgba(0, 158, 251, 0.50)");
+    gradientFill.addColorStop(1, "rgba(0, 158, 251, 0.04)");
+
+    // all data
+    var data_1 = [1800, 1600, 2300, 2800, 3600, 2900, 3000, 3800, 3600];
+    var data_2 = [4100, 3800, 3200, 3400, 2700, 2600, 3300, 3000, 2900];
+    var labels = ["June-11", "Jan-12", "Jan-13", "Jan-14", "Jan-15", "Jan-16", "Jan-17", "Jan-18", "Jan-19"];
+
+    var lineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: "Patient",
+                borderColor: gradientStroke,
+                pointBorderColor: gradientStroke,
+                pointBackgroundColor: gradientStroke,
+                pointHoverBackgroundColor: gradientStroke,
+                pointHoverBorderColor: gradientStroke,
+                pointBorderWidth: 1,
+                pointHoverRadius: 4,
+                pointHoverBorderWidth: 1,
+                pointRadius: 2,
+                fill: true,
+                backgroundColor: gradientFill,
+                borderWidth: 1,
+                data: data_1
+            }]
+        },
+        options: {
+            legend: {
+                display: false,
+                position: "bottom"
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        fontColor: "rgba(0,0,0,0.5)",
+                        fontStyle: "bold",
+                        beginAtZero: true,
+                        maxTicksLimit: 200,
+                        padding: 20
+                    },
+                    gridLines: {
+                        drawTicks: false,
+                        display: false
+                    }
+
+                }],
+                xAxes: [{
+                    gridLines: {
+                        zeroLineColor: "transparent"
+                    },
+                    ticks: {
+                        padding: 20,
+                        fontColor: "rgba(0,0,0,0.5)",
+                        fontStyle: "bold"
+                    }
+                }]
+            }
+        }
+    });
+
+        const diagnosisAndRisk = await funDiagnosisRisk();
     console.log(diagnosisAndRisk);
 
     let diagnosisData = [];
@@ -245,75 +315,6 @@ async function funDiagnosisRisk() {
         }
     });
 
-
-    //Line Chart
-    var ctx = document.getElementById('line-chart').getContext("2d");
-    var gradientStroke = ctx.createLinearGradient(0, 0, 0, 450);
-    gradientStroke.addColorStop(0, '#009efb');
-
-    var gradientFill = ctx.createLinearGradient(0, 0, 0, 450);
-    gradientFill.addColorStop(0, "rgba(0, 158, 251, 0.50)");
-    gradientFill.addColorStop(1, "rgba(0, 158, 251, 0.04)");
-
-    // all data
-    var data_1 = [1800, 1600, 2300, 2800, 3600, 2900, 3000, 3800, 3600];
-    var data_2 = [4100, 3800, 3200, 3400, 2700, 2600, 3300, 3000, 2900];
-    var labels = ["Jan-11", "Jan-12", "Jan-13", "Jan-14", "Jan-15", "Jan-16", "Jan-17", "Jan-18", "Jan-19"];
-
-    var lineChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: "Patient",
-                borderColor: gradientStroke,
-                pointBorderColor: gradientStroke,
-                pointBackgroundColor: gradientStroke,
-                pointHoverBackgroundColor: gradientStroke,
-                pointHoverBorderColor: gradientStroke,
-                pointBorderWidth: 1,
-                pointHoverRadius: 4,
-                pointHoverBorderWidth: 1,
-                pointRadius: 2,
-                fill: true,
-                backgroundColor: gradientFill,
-                borderWidth: 1,
-                data: data_1
-            }]
-        },
-        options: {
-            legend: {
-                display: false,
-                position: "bottom"
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        fontColor: "rgba(0,0,0,0.5)",
-                        fontStyle: "bold",
-                        beginAtZero: true,
-                        maxTicksLimit: 200,
-                        padding: 20
-                    },
-                    gridLines: {
-                        drawTicks: false,
-                        display: false
-                    }
-
-                }],
-                xAxes: [{
-                    gridLines: {
-                        zeroLineColor: "transparent"
-                    },
-                    ticks: {
-                        padding: 20,
-                        fontColor: "rgba(0,0,0,0.5)",
-                        fontStyle: "bold"
-                    }
-                }]
-            }
-        }
-    });
 
 
     //Line Chart 2
